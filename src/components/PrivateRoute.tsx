4b9e3577668b4218cfe84b6fc1969325d3421ccb
Sullygrrr <eesu7232004@gmail.com>
2024-12-01 21:11:10 +0100

    Delete src directory

--- src/components/PrivateRoute.tsx
+++
@@ -1,8 +0,0 @@
-import { Navigate } from 'react-router-dom';
-import { useAuth } from '../contexts/AuthContext';
-
-export const PrivateRoute: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  const { currentUser } = useAuth();
-  
-  return currentUser ? <>{children}</> : <Navigate to="/login" />;
-};
--- src/utils/generateUsername.ts
+++
@@ -1,8 +0,0 @@
-import { customAlphabet } from 'nanoid';
-
-// Créer un générateur avec uniquement des lettres et des chiffres
-const nanoid = customAlphabet('abcdefghijklmnopqrstuvwxyz0123456789', 8);
-
-export const generateDefaultUsername = () => {
-  return `user${nanoid()}`;
-};
